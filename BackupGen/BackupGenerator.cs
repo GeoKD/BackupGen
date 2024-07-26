56f83de25566c7f6f8fa587081186de05586499b
GeoKD <gioman5817@gmail.com>
2024-07-26 20:41:14 +0300

    Getting ready

--- BackupGen/BackupGenerator.cs
+++ BackupGen/BackupGenerator.cs
@@ -1,6 +1,30 @@
+using BackupGen.Settings;
+
 namespace BackupGen;
 
 public class BackupGenerator
 {
-    
+    private SetingsOptions _settingsOptions;
+
+    public BackupGenerator(SetingsOptions settingsOptions)
+    {
+        _settingsOptions = settingsOptions;
+    }
+
+    public void CopyFiles()
+    {
+        foreach (var folder in _settingsOptions.sourceFolders)
+        {
+            if (Directory.Exists(folder))
+            {
+                CopyFolder(folder);
+            }
+        }
+    }
+
+    private void CopyFolder(string folderPath)
+    {
+        string[] files = Directory.GetFiles(folderPath);
+    }
+
 }
